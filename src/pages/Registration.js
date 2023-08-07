673e7c62e51380a50b7386e659eead2b317dfb57
Ludovico Pio Gnerre <ellpgi@gmail.com>
2023-08-07 13:03:25 +0200

    error message

--- src/pages/Registration.js
+++ src/pages/Registration.js
@@ -2,14 +2,15 @@ import React from 'react'
 import { useState } from 'react'
 import {auth} from '../firebase_setup/firebase.js'
 import {createUserWithEmailAndPassword} from 'firebase/auth'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 
 export default function Registration() {
 
-    const [email, setEmail] = useState('')
+  const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
   const [confirmPassword, setConfirmPassword] = useState('')
   const [error, setError] = useState('')
+  const navigate = useNavigate();
 
      const validatePassword = () => {
          let isValid = true
@@ -29,12 +30,13 @@ export default function Registration() {
             createUserWithEmailAndPassword(auth, email, password)
             .then((res) => {
                 console.log(res.user)
+                navigate('/home');
               })
-            .catch(err => setError(err.message))
+            .catch(err => setError('Email assente'))
         }
-        setEmail('')
-        setPassword('')
-        setConfirmPassword('')
+        // setEmail('')
+        // setPassword('')
+        // setConfirmPassword('')
       }
 
   return (
@@ -71,15 +73,22 @@ export default function Registration() {
 <div className="md:w-2/3">
   <input className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-confirm-password" type="password" onChange={e => setConfirmPassword(e.target.value)} value={confirmPassword} placeholder="******************"/>
 </div>
+
 </div>
 
-<div className="md:flex md:items-center">
-
-  
+<div className="md:flex flex-col items-end md:items-center ">
+  <div>
+   {error && <h1 className='text-red-500' >{error}</h1>}
+  </div>
+  <div class="flex flex-row-reverse mt-4">
   <Link to="/"><button className="shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded" type="button">
     Indietro
   </button>
   </Link>
+  <button className="shadow mr-4 bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded" type="submit">
+    Registrati
+  </button>
+  </div>
 </div>
 <div className="flex mt-4 justify-center">
  
